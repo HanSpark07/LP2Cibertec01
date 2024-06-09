7c5434eb561b14924e8b66f71f5ed4ebb56a1b73
Hans <145622701+HanSpark07@users.noreply.github.com>
2024-06-09 16:53:43 -0500

    LP2_Lab1_Velasquez_Hans
    Proyecto en IntelliJ e imágenes de su funcionamiento en Postman.

---
+++ Lab1HV/src/test/java/com/cibertec/Lab1HV/Lab1HvApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cibertec.Lab1HV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class Lab1HvApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1HvApplicationTests.class, args);
+	}
+
+}
