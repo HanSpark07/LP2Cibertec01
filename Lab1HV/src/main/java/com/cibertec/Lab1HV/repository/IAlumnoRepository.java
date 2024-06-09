64c97fd82978704852812736c6ae05e006ee55a4
Hans <145622701+HanSpark07@users.noreply.github.com>
2024-06-09 16:51:54 -0500

    Delete Lab1HV directory

--- Lab1HV/src/main/java/com/cibertec/Lab1HV/repository/IAlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.Lab1HV.repository;
-
-import com.cibertec.Lab1HV.model.Alumno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IAlumnoRepository extends JpaRepository<Alumno, Integer> {
-}
